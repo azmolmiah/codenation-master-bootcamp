2af9e500e6479941d20302bd2d5d459524409381
azmolmiah <azmol.miah.general@gmail.com>
2022-06-20 14:20:11 +0100

    Fixed directory issues as ./movies for fs.fileReadSync was giving an error that file could not be found and used __dirname/movies.json so that way this can be used on any machine and not the path to my machine only

--- src/app.js
+++ src/app.js
@@ -2,18 +2,13 @@ const fs = require("fs");
 const Movie = require("./utils");
 const input = process.argv;
 
-const jsonFile = fs.readFileSync(
-  "/Users/codenation/Documents/Master38/WeekSeven/src/movies.json"
-);
+const jsonFile = fs.readFileSync(__dirname + "/movies.json");
 let json = JSON.parse(jsonFile);
 
 if (input[2] === "add") {
   const movie = new Movie(input[3], input[4]);
   json.push(movie.add());
-  fs.writeFileSync(
-    "/Users/codenation/Documents/Master38/WeekSeven/src/movies.json",
-    JSON.stringify(json)
-  );
+  fs.writeFileSync(__dirname + "/movies.json", JSON.stringify(json));
 }
 
 console.log(json);
